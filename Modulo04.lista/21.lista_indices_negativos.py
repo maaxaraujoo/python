964926b4066d99c4c5ac0407f4f68d226505508b
maaxaraujoo <max.araujo.gomes29@gmail.com>
2026-09-09 18:10:55 +0000

    at

---
+++ Modulo04.lista/21.lista_indices_negativos.py
@@ -0,0 +1,19 @@
+# 21.lista_indices_negativos.py
+lista = ["Banana","Melancia","Maça","Uva","Banana","Uva Verde"]
+print(f"Último elemento: {lista[-1]}")
+print(f"Penúltimo elemento: {lista[-2]}")
+print(f"Antepenúltimo elemento: {lista[-3]}")
+# QUESTÃO 21 — ÍNDICES NEGATIVOS
+#
+# Crie uma lista contendo 6 frutas.
+#
+# Requisitos:
+#
+# - Mostre o último elemento utilizando índice negativo.
+# - Mostre o penúltimo elemento utilizando índice negativo.
+# - Mostre o terceiro elemento a partir do final utilizando
+#   índice negativo.
+#
+# Objetivo:
+# - Praticar índices negativos.
+# - Entender como acessar elementos começando pelo final da lista.
