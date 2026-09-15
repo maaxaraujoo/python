809a3d78fa836902b6619c019a9c8bdf119416e6
Maxwell <max.araujo.gomes29@gmail.com>
2026-09-15 15:47:44 -0300

    Novas atualizações

---
+++ Modulo05.strings/16.string_concatenacao_repeticao.py
@@ -0,0 +1,45 @@
+# 16.string_concatenacao_repeticao.py
+nome = "Gabriel"
+sobre = "Ferreira"
+nomecomple = nome +" "+sobre
+print("----------------Impressões----------------")
+print(f"Nome:{nome}")
+print(f"Sobreno:{sobre}")
+print(f"Nome completo: {nomecomple}")
+palavra = "K"
+print(f"Eu amo rir: {palavra*3}")
+print("------------------------------------------")
+
+
+
+# QUESTÃO 16 — Concatenando e repetindo Strings
+#
+# Crie duas variáveis:
+#
+# nome = "Max"
+# sobrenome = "Gomes"
+#
+# O programa deve:
+#
+# 1. Mostrar o nome.
+#
+# 2. Mostrar o sobrenome.
+#
+# 3. Utilizar o operador + para juntar o nome e o sobrenome,
+#    colocando um espaço entre eles.
+#
+# 4. Armazenar o resultado em uma nova variável.
+#
+# 5. Mostrar o nome completo.
+#
+# 6. Criar uma variável chamada palavra contendo:
+#    "Python"
+#
+# 7. Utilizar o operador * para repetir a palavra "Python"
+#    exatamente 3 vezes.
+#
+# 8. Mostrar o resultado da repetição.
+#
+# OBJETIVO:
+# Praticar concatenação de Strings utilizando + e repetição
+# de Strings utilizando *.
