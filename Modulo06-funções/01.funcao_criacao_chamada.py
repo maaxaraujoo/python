d1bd86952808c2bfedfc0ac0037fb51652d03ce2
maaxaraujoo <max.araujo.gomes29@gmail.com>
2026-09-16 15:58:08 -0300

    Create and call 'saudacao'
    Implement the 'saudacao' function to greet users.

---
+++ Modulo06-funções/01.funcao_criacao_chamada.py
@@ -0,0 +1,19 @@
+# 01.funcao_criacao_chamada.py
+def saudacao():
+    print("Olá, seja bem vindo ao seu novo módulo do curso!")
+saudacao()
+
+# QUESTÃO 01 — Criando e chamando uma função
+#
+# Crie uma função chamada saudacao().
+#
+# A função deve:
+#
+# 1. Mostrar na tela a mensagem:
+#    "Olá! Seja bem-vindo ao Python."
+#
+# 2. Depois de criar a função, chamar a função 3 vezes.
+#
+# OBJETIVO:
+# Praticar a criação de uma função utilizando def e sua
+# execução através da chamada da função.
