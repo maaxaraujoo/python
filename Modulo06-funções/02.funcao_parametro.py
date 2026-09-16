a32d2e7bf07308f5033ed4ab0ea7a69dbc93b6c5
maaxaraujoo <max.araujo.gomes29@gmail.com>
2026-09-16 16:50:26 -0300

    Create 02.funcao_parametro.py

---
+++ Modulo06-funções/02.funcao_parametro.py
@@ -0,0 +1,30 @@
+# 02.funcao_parametro.py
+def saudacao(nome):
+	print(f"Seja bem vindo {nome}!")
+	
+saudacao("Maxwell")
+saudacao("Gabriel")
+
+# QUESTÃO 02 — Utilizando parâmetros em funções
+#
+# Crie uma função chamada saudacao() que receba um parâmetro
+# chamado nome.
+#
+# O programa deve:
+#
+# 1. Fazer a função receber o nome de uma pessoa através
+#    do parâmetro.
+#
+# 2. Mostrar na tela uma mensagem de saudação utilizando
+#    o nome recebido.
+#
+# 3. Chamar a função utilizando o nome:
+#    "Maxwell"
+#
+# 4. Chamar novamente a função utilizando o nome:
+#    "Gabriel"
+#
+# OBJETIVO:
+# Praticar a criação de funções com parâmetros e entender
+# como valores podem ser enviados para uma função no momento
+# da chamada.
