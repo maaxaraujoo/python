2f105d6c6390128b14480007b6b8fd4a12d4fff8
maaxaraujoo <max.araujo.gomes29@gmail.com>
2026-09-16 16:59:11 -0300

    Create apresentar function with multiple parameters
    Implement function to present name and age

---
+++ Modulo06-funções/# 03.funcao_multiplos_parametros.py
@@ -0,0 +1,31 @@
+# 03.funcao_multiplos_parametros.py
+#
+# QUESTÃO 03 — Função com múltiplos parâmetros
+def apresentar(nome,idade):
+  print(f"Seu nome é muito bonito, {nome}")
+  print(f"Caramba, vocẽ tem apenas {idade} anos?")
+  print("--------------------------------------------------")
+  
+apresentar("Maxwell",22)
+apresentar("Gabriel",20)
+
+# Crie uma função chamada apresentar().
+#
+# A função deve receber dois parâmetros:
+# nome e idade.
+#
+# O programa deve:
+#
+# 1. Mostrar o nome recebido.
+#
+# 2. Mostrar a idade recebida.
+#
+# 3. Chamar a função passando:
+#    "Maxwell" e 22
+#
+# 4. Chamar novamente a função passando:
+#    "Gabriel" e 20
+#
+# OBJETIVO:
+# Praticar a criação de funções com mais de um parâmetro
+# e o envio de múltiplos argumentos durante a chamada.
