7af315e014652e062c0f7731cabf22091eda2053
Maxwell <max.araujo.gomes29@gmail.com>
2026-09-17 14:52:05 -0300

    atualizações

--- 02.PYHON - IFMG/SEMANA 01/s01_item_a.py
+++
@@ -1,6 +0,0 @@
-print("------------------ÁREA DE UM TRIÂNGULO------------------")
-base = int(input("Qual a base do triângulo desejado?"))
-altura = int(input("Qual a altura do triângulo desejado?"))
-area = (base * altura)/2
-print(f"A área do triângulo é: {area}")
-print("-------------------------FIM-----------------------------")
