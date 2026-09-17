d9e73270d9e42e58c37744ef5d3fbd021cc11b0a
Maxwell <max.araujo.gomes29@gmail.com>
2026-09-17 14:49:04 -0300

    semana1

---
+++ 02.PYHON_IFMG/SEMANA01/s01_item_b.py
@@ -0,0 +1,8 @@
+print("------------------CONVERSÃO SEGUNDO/HORA/MINUTO------------------")
+segun = int(input("Digite os segundos que desejas ver a conversão:"))
+minut = segun * 60
+hora = segun/3600
+print(f"Valor de segundos: {segun} ")
+print(f"{segun} segundos em minutos é igual a: {minut}")
+print(f"{segun} segundos em horas é igual a: {hora}")
+print("-------------------------FIM-----------------------------")
