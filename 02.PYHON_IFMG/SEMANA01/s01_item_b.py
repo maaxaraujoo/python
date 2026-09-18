6a6f86a86759d85b10e2bd571c23ca699926d306
maaxaraujoo <max.araujo.gomes29@gmail.com>
2026-09-18 09:34:42 -0300

    Add conversion for hours to seconds and minutes

--- 02.PYHON_IFMG/SEMANA01/s01_item_b.py
+++ 02.PYHON_IFMG/SEMANA01/s01_item_b.py
@@ -1,7 +1,15 @@
-print("------------------CONVERSÃO SEGUNDO/HORA/MINUTO------------------")
+print("------------------CONVERSÃO SEGUNDO/MINUTO/HORA------------------")
 segun = int(input("Digite os segundos que desejas ver a conversão:"))
 minut = segun * 60
 hora = segun/3600
+print(f"Valor de segundos: {segun} ")
+print(f"{segun} segundos em minutos é igual a: {minut}")
+print(f"{segun} segundos em horas é igual a: {hora}")
+print("-------------------------FIM-----------------------------")
+print("------------------CONVERSÃO HORA/MINUTO/SEGUNDO------------------")
+hora = int(input("Digite as horas que desejas ver a conversão:"))
+minut = hora*60
+segun = hora*3600
 print(f"Valor de segundos: {segun} ")
 print(f"{segun} segundos em minutos é igual a: {minut}")
 print(f"{segun} segundos em horas é igual a: {hora}")
