731f56d2642808d6ef9b450506ba21b6e93bc52e
maaxaraujoo <max.araujo.gomes29@gmail.com>
2026-09-18 09:06:12 -0300

    Add simple interest calculation script

---
+++ 02.PYHON_IFMG/SEMANA01/s01_item_c.py
@@ -0,0 +1,8 @@
+print("----------CÁLCULO DE JUROS SIMPLES----------")
+c = int(input("Digite o valor de C:"))
+i = int(input("Digite o valor de I:"))
+t = int(input("Digite o valor de T:"))
+
+J = (c*i*t)/100
+print(f"O valor de J é igual a:{J}")
+print("-------------------FIM----------------------")
