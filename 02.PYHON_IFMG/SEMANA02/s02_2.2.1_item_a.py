ff22af827f191950ec7616ac3913226162cf04dd
maaxaraujoo <max.araujo.gomes29@gmail.com>
2026-09-19 09:59:21 -0300

    Add logic to find the largest of three numbers

--- 02.PYHON_IFMG/SEMANA02/s02_2.2.1_item_a.py
+++ 02.PYHON_IFMG/SEMANA02/s02_2.2.1_item_a.py
@@ -1,1 +1,16 @@
+print("---------------NÚMERO MAIOR---------------")
+a = int(input("Digite o 1º número:"))
+b = int(input("Digite o 2º número:"))
+c = int(input("Digite o 3º número:"))
+
+aux = a #10
+
+if aux < b: # 10 < 5 - falso
+  aux = b
+if aux < c: # 15 < 20 - verdadeiro
+  aux = c
+
+print(f"O maior número entre os três é:{aux}")
+print("------------------------------------------")
+
 
