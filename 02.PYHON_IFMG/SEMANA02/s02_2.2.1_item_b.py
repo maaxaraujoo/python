d5f7fbf023049290258b73efd684251edf10da35
maaxaraujoo <max.araujo.gomes29@gmail.com>
2026-09-19 10:13:08 -0300

    Add script to check if a number is even or odd

---
+++ 02.PYHON_IFMG/SEMANA02/s02_2.2.1_item_b.py
@@ -0,0 +1,7 @@
+print("-------------------NÚMERO PAR E ÍMPAR-------------------")
+numero = int(input("Digite um número:"))
+if numero / 2 == numero // 2:
+  print("Esse número é par.")
+else:
+  print("Esse número é ímpar.")
+print("------------------------ FIM! ------------------------")
